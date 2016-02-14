c1efc2aadf74ffa634504263ae82d92218cbb1b4
jpchamps <juanpablo.camposg>
2016-02-14 16:23:07 -0500

    completed interactive example

--- app examples/app_example3/Server.R
+++ app examples/app_example3/Server.R
@@ -2,17 +2,23 @@ library(choroplethrZip)
 data(zip.regions)
 ny <- subset(zip.regions,zip.regions$county.fips.numeric==36061)
 ny.region <- ny$region
-data.zip <- data.frame(region=ny.region,value=rnorm(length(ny.region),0,1),stringsAsFactors = F)
+n <- length(ny.region)
+values <- data.frame(array(runif(3*n,0,100),dim = c(n,3)))
+names(values) <-  c("price","rating","schools")
+data.zip <- data.frame(ny.region,values,stringsAsFactors = F)
+f.variable <- function(x){switch(x,'1'="price",'2'="rating",'3'="schools")}
+
 
 shinyServer(
   function(input,output){
-    set.seed(122)
-    histdata <- rnorm(500)
-    
+    data.zip.red <- data.zip
     output$plot1 <- renderPlot({
-      zip_choropleth(data.zip, title = "2009 Manhattan housing sales", legend = "Number of sales", county_zoom = 36061, reference_map = T)
+      s.variable=f.variable(input$variable)
+      data.zip.red[data.zip$price<input$d.price[1] | data.zip$price>input$d.price[2],s.variable] <- NA
+      data.zip.red[data.zip$rating<input$d.rating[1] | data.zip$rating>input$d.rating[2],s.variable] <- NA
+      data.zip.red[data.zip$schools<input$d.schools[1] | data.zip$schools>input$d.schools[2],s.variable] <- NA
+      data.plot <- data.frame(region=data.zip.red$ny.region, value=data.zip.red[,s.variable],stringsAsFactors = F)
+      zip_choropleth(data.plot, title = "Manhattan housing", legend = s.variable, county_zoom = 36061, reference_map = F)
     })
-    output$range <- renderText(paste0(input$price[1]))
-    
   }
 )
--- app examples/app_example3/ui.R
+++ app examples/app_example3/ui.R
@@ -2,10 +2,10 @@ library(shinydashboard)
 
 shinyUI(
   dashboardPage(skin="yellow",
-    dashboardHeader(title = "Restaurants"),
+    dashboardHeader(title = "Find your home!"),
     dashboardSidebar(
       sidebarMenu(
-        menuItem("Introduction", tabName = "introduction", icon = icon("hand-peace")),
+        menuItem("Introduction", tabName = "introduction", icon = icon("hand-peace-o")),
         menuItem("Explore NYC", tabName = "explore", icon = icon("map")),
         menuItem("Survey",tabName="survey",icon=icon("paper-plane"))
       )
@@ -18,14 +18,19 @@ shinyUI(
         ),
         tabItem(tabName = "explore",
                 fluidPage(center=T,
-                  h1("this is the title"),
-                  sliderInput("price", "Number of observations:", 1, 100, c(0,100)),
-                  plotOutput("plot1", height = 1000),
-                  textOutput("range")
+                  h1("Find the most suitable home for you!"),
+                  radioButtons("variable", label = h3("Choose the variable you want to visualize"), choices = list("Price" = 1, "Rating" = 2, "Schools" = 3), selected = 1),
+                  fluidRow(
+                    column(4,sliderInput("d.price", "Price:", 0, 100, c(0,100))),
+                    column(4,sliderInput("d.rating", "Rating:", 0, 100, c(0,100))),
+                    column(4,sliderInput("d.schools", "Number of schools:", 0, 100, c(0,100)))
+                  ),
+                  
+                  plotOutput("plot1", height = 1000)
                   
 #                   box(
 #                     title = "Price",
-#                     sliderInput("price", "Number of observations:", 1, 100, c(0,100))
+#                     sliderInput("price", "Price range:", 0, 100, c(0,100))
 #                   )
                 )
         ),
